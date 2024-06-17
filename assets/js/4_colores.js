0da5c55231f82ef30ce578c45563e01be748e74c
Daniel Molina <danielmolina9202@gmail.com>
2024-06-17 16:23:12 -0400

    tercer ejercicio realizado

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -3,7 +3,7 @@ const div2 = document.querySelector('#div2')
 const div3 = document.querySelector('#div3')
 const div4 = document.querySelector('#div4')
 
-function sizeDiv (elemento,alto,ancho,background) {
+function sizeDiv(elemento, alto, ancho, background) {
     elemento.style.height = alto
     elemento.style.width = ancho
     elemento.style.backgroundColor = background
@@ -14,7 +14,11 @@ sizeDiv(div2, '200px', '200px', 'red');
 sizeDiv(div3, '200px', '200px', 'green');
 sizeDiv(div4, '200px', '200px', 'yellow');
 
+function cambiarColor(elemento) {
+    elemento.style.backgroundColor = 'black';
+}
 
-
-
-
+div1.addEventListener("click", () => cambiarColor(div1));
+div2.addEventListener("click", () => cambiarColor(div2));
+div3.addEventListener("click", () => cambiarColor(div3));
+div4.addEventListener("click", () => cambiarColor(div4));

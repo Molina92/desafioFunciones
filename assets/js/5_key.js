fc1eeab4e666a4f64394d8a9be5ad6edc617ae03
Daniel Molina <danielmolina9202@gmail.com>
2024-06-17 17:22:09 -0400

    ultimo ejercicio en desarrollo

---
+++ assets/js/5_key.js
@@ -0,0 +1,11 @@
+const key = document.querySelector('#key')
+
+function aspectDiv(elemento) {
+    elemento.style.height = '200px'
+    elemento.style.width = '200px'
+    elemento.style.backgroundColor = 'black'
+}
+
+aspectDiv(key)
+
+
